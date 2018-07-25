2b1c2b8c1356c696495aaba1efab3706b7074d26
Danny Mora <dannyf620@gmail.com>
2018-07-24 21:08:44 -0500

    estudiante router

--- index.js
+++ index.js
@@ -6,62 +6,12 @@ const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
+const estudianteRouter = require('./routes/estudianteRouter');
 
 const app = express();
 app.use(morgan('dev'));
-
+app.use('/profesores', estudianteRouter);
 app.use(express.static(__dirname + '/public'));
-
-// app.use((req, res, next) => {
-//   console.log(req.headers);
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/html');
-//   res.end('<html><body><h1>Mi servidor creado cno Express</h1></body></html>');
-
-// });
-
-app.use(bodyParser.json());
-
-app.all('/estudiantes', (req,res,next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-});
-
-app.get('/estudiantes', (req,res,next) => {
-    res.end('Este metodo retornara la lista de estudiantes');
-});
-app.post('/estudiantes', (req, res, next) => {
-    console.dir(req.body);
- res.end('Se agregare el estudiante ' + req.body.name + ' que vive en : ' + req.body.address);
-});
-app.put('/estudiantes', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('el metodo PUT no es soportado en  /estudiantes');
-}); 
-app.delete('/estudiantes', (req, res, next) => {
-    res.end('Eliminando todos los estudiantes');
-});
-
-app.get('/estudiantes/:estudianteId', (req,res,next) => {
-    res.end('Envia todos los detalles del estudiante con id: ' + req.params.estudianteId );
-});
-
-app.post('/estudiantes/:estudianteId', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('la operacion POST no es soportada en  /estudiantes/'+ req.params.estudianteId);
-});
-
-app.put('/estudiantes/:estudianteId', (req, res, next) => {
-  res.write('Actualizando datos del  estudiante: ' + req.params.estudianteId + '\n');
-  res.end('Se actualizara al estudiante: ' + req.body.name + 
-        ' con direccion: ' + req.body.address);
-});
-
-app.delete('/estudiantes/:estudianteId', (req, res, next) => {
-    res.end('Eliminando al estudiante: ' + req.params.estudianteId);
-});
-
 
 const server = http.createServer(app);
 
